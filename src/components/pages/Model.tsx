f869dab0fb69486202918fed60dfcb946f416e3b
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-01-10 08:24:32 +0100

    added model for all pages

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
-import {adidasArr} from './ADIDAS';
-import {pumaArr} from './PUMA';
+import { useParams } from 'react-router-dom';
+import { adidasArr } from './ADIDAS';
+import { pumaArr } from './PUMA';
+import {nikeArr} from './NIKE';
 
 export const Model: React.FC = () => {
     const params = useParams();
-    const modelId = Number(params.id) - 1;
+    const modelId = Number(params.id);
+
+    // Находим текущую модель
+    const modelAdidas = adidasArr.find(el => el.id === modelId);
+    const modelPuma = pumaArr.find(el => el.id === modelId);
+    const modelNike = nikeArr.find(el => el.id === modelId);
+
     return (
-        <div style={{textAlign: 'center'}}>
-            <h2>{adidasArr[modelId].model}</h2>
-            <h3>{adidasArr[modelId].collection}</h3>
-            <h4>{adidasArr[modelId].price}</h4>
-            <img src={adidasArr[modelId].picture} alt="adidas"/>
-{/*
-
-            <h2>{pumaArr[modelId].model}</h2>
-            <h3>{pumaArr[modelId].collection}</h3>
-            <h4>{pumaArr[modelId].price}</h4>
-            <img src={pumaArr[modelId].picture} alt="adidas"/>*/}
+        <div style={{ textAlign: 'center' }}>
+            {modelAdidas ? (
+                <>
+                    <h2>{modelAdidas.model}</h2>
+                    <h3>{modelAdidas.collection}</h3>
+                    <h4>{modelAdidas.price}</h4>
+                    <img src={modelAdidas.picture} alt={modelAdidas.model} />
+                </>
+            ) : modelPuma ?
+                <>
+                    <h2>{modelPuma.model}</h2>
+                    <h3>{modelPuma.collection}</h3>
+                    <h4>{modelPuma.price}</h4>
+                    <img src={modelPuma.picture} alt={modelPuma.model} width={'800px'} height={'500px'} />
+                </>
+                : modelNike ?
+                    <>
+                        <h2>{modelNike.model}</h2>
+                        <h3>{modelNike.collection}</h3>
+                        <h4>{modelNike.price}</h4>
+                        <img src={modelNike.picture} alt={modelNike.model} width={'800px'} />
+                    </>
+                : (
+                <h2>Такого товара нет в наличии</h2>
+            )}
         </div>
-
-
     );
 };
