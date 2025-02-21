9dc330973d89b14f78a09fc097cbb273bbea96f0
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-02-21 16:46:48 +0100

    create button 'На главную'

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {NavLink, Outlet} from 'react-router-dom';
+import {Link, NavLink, Outlet} from 'react-router-dom';
 import styles from "./components/Site.module.css";
 import {S} from './components/pages/_styles';
 
@@ -13,20 +13,24 @@ const PATH = {
 } as const;
 
 function App() {
-    return <><div className={styles.header}>
-        <h1>HEADER</h1>
-    </div>
-        <div className={styles.body}>
-        <div className={styles.nav}>
-            <S.NavWrapper><NavLink to={PATH.ADIDAS}>Adidas</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink to={PATH.PUMA}>Puma</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink to={PATH.NIKE}>Nike</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink to={PATH.PRICES}>Price</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink to={PATH.PROTECTEDPAGE}>ProtectedPage</NavLink></S.NavWrapper>
-            <S.NavWrapper><NavLink to={PATH.LOGIN}>Login</NavLink></S.NavWrapper>
+    return <>
+        <div className={styles.header}>
+            <h1>HEADER</h1>
         </div>
+
+        <div className={styles.body}>
+            <div className={styles.nav}>
+                <S.NavWrapper><NavLink to={PATH.ADIDAS}>Adidas</NavLink></S.NavWrapper>
+                <S.NavWrapper><NavLink to={PATH.PUMA}>Puma</NavLink></S.NavWrapper>
+                <S.NavWrapper><NavLink to={PATH.NIKE}>Nike</NavLink></S.NavWrapper>
+                <S.NavWrapper><NavLink to={PATH.PRICES}>Price</NavLink></S.NavWrapper>
+                <S.NavWrapper><NavLink to={PATH.PROTECTEDPAGE}>ProtectedPage</NavLink></S.NavWrapper>
+                <S.NavWrapper><NavLink to={PATH.LOGIN}>Login</NavLink></S.NavWrapper>
+            </div>
+
             <div className={styles.content}>
-                <Outlet />
+                <Link to={PATH.ADIDAS} className={styles.LinkLikeButton}>На главную</Link>
+                <Outlet/>
             </div>
         </div>
         <div className={styles.footer}>Colection 2025
